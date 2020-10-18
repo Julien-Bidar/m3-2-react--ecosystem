91c12bce4028bac24948143d1e39c2e43b793017
Julien Bidar <julien.bidar@gmail.com>
2020-10-17 22:28:49 -0400

    style tweaks in Nav

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,25 +3,61 @@ import { NavLink } from "react-router-dom";
 import styled from "styled-components";
 
 const HeaderStyled = styled.div`
-  display: inline-flex;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding-top: 25px;
+`;
+
+const NavWrapper = styled.div`
+  display: flex;
 `;
 
 const Title = styled.h1`
   font-family: sans-serif;
 `;
 
-const StyledNavLink = styled(NavLink)
+const StyledNavLink = styled(NavLink)`
+  position: relative;
+  text-decoration: none;
+  padding: 0 16px;
+
+  &.active {
+    color: rebeccapurple;
+  }
+  &:after {
+    content: "";
+    position: absolute;
+    background-color: currentColor;
+    left: 0;
+    right: 0;
+    bottom: -5px;
+    width: 50%;
+    margin: auto;
+    height: 3px;
+    transform: scaleX(0);
+    transform-origin: center center;
+    border-radius: 2px;
+  }
+  &.active:after {
+    transition: transform 250ms, opacity 150ms;
+    transform: scaleX(1);
+    opacity: 1;
+  }
+`;
 
 const Header = (props) => {
   return (
     <HeaderStyled>
       <Title>Fruit Emporium</Title>
-      <NavLink to="/">
-        <p>Home</p>
-      </NavLink>
-      <NavLink to="/about">
-        <p>About</p>
-      </NavLink>
+      <NavWrapper>
+        <StyledNavLink exact activeClassName="active" to="/">
+          Home
+        </StyledNavLink>
+        <StyledNavLink activeClassName="active" to="/about">
+          About
+        </StyledNavLink>
+      </NavWrapper>
     </HeaderStyled>
   );
 };
